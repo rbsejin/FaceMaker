057bce7024404dc25a440832ba1e23c783cab5f6
rbsejin <rbsejin@gmail.com>
2021-02-14 07:54:20 +0900

    필요 없는 코드 제거

--- app/src/main/java/com/example/facemaker/TaskDetailActivity.kt
+++ app/src/main/java/com/example/facemaker/TaskDetailActivity.kt
@@ -18,14 +18,12 @@ class TaskDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_detail)
 
-        var currentTaskId: Long? = null
-
         /* Connect variables to UI elements. */
         val taskContent: TextView = findViewById(R.id.task_detail_content)
         val taskCheckBox: CheckBox = findViewById(R.id.task_detail_checkBox)
 
         val bundle: Bundle? = intent.extras
-        currentTaskId = bundle?.getLong(TASK_ID)
+        var currentTaskId : Long? = bundle?.getLong(TASK_ID)
 
         currentTaskId?.let {
             val currentTask = TaskManager.getTaskForId(it)
