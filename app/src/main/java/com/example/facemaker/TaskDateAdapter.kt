95ab6a1bca07d12bca62610ef064d370af5e74b2
rbsejin <rbsejin@gmail.com>
2021-02-15 13:07:28 +0900

    Task Detail 에서 아이템 클릭시 팝업 메뉴 띄우기 구현

--- app/src/main/java/com/example/facemaker/TaskDateAdapter.kt
+++ app/src/main/java/com/example/facemaker/TaskDateAdapter.kt
@@ -1,16 +1,16 @@
 package com.example.facemaker
 
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.annotation.DrawableRes
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 
-class TaskDateAdapter() :
+class TaskDateAdapter :
     Adapter<TaskDateAdapter.TaskDateViewHolder>() {
     private val taskDateList: List<TaskDateItem> = listOf(
         TaskDateItem(R.drawable.baseline_notifications_none_black_24, "미리 알림"),
@@ -18,7 +18,9 @@ class TaskDateAdapter() :
         TaskDateItem(R.drawable.baseline_repeat_black_24, "반복")
     )
 
-    class TaskDateViewHolder(itemView: View, onClick: (TaskDateItem) -> Unit) :
+    private lateinit var parentContext: Context
+
+    class TaskDateViewHolder(itemView: View) :
         RecyclerView.ViewHolder(itemView) {
         val icon: ImageView = itemView.findViewById(R.id.task_date_item_icon)
         val content: TextView = itemView.findViewById(R.id.task_date_item_content)
@@ -27,9 +29,7 @@ class TaskDateAdapter() :
 
         init {
             itemView.setOnClickListener {
-                currentTaskDateItem?.let {
-                    onClick(it)
-                }
+
             }
 
             close.visibility = View.INVISIBLE
@@ -48,28 +48,43 @@ class TaskDateAdapter() :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskDateViewHolder {
+        parentContext = parent.context
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.task_date_item, parent, false)
-        return TaskDateViewHolder(view) {taskDateItem -> adapterOnClick(taskDateItem) }
+        return TaskDateViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TaskDateViewHolder, position: Int) {
         holder.bind(taskDateList[position])
+
+        holder.itemView.setOnClickListener {
+            val popup = PopupMenu(parentContext, holder.itemView)
+            val inflater: MenuInflater = popup.menuInflater
+            inflater.inflate(R.menu.date_item_menu, popup.menu)
+            popup.setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.today_later_item -> {
+                        Toast.makeText(parentContext, "today", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.tomorrow_item -> {
+                        true
+                    }
+                    R.id.next_week_item -> {
+                        true
+                    }
+                    R.id.direct_selection_item -> {
+                        true
+                    }
+                    else -> false
+                }
+            }
+            popup.show()
+        }
     }
 
     override fun getItemCount(): Int {
         return 3
-    }
-
-    fun adapterOnClick(taskDateItem: TaskDateItem) {
-        taskDateItem?.let {
-            // contextMenu 생성
-            // 클릭시
-
-            notifyDataSetChanged()
-        }
-
-        // close 클릭시 원상복구
     }
 }
 
