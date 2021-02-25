895419bcc671fd616069e10560be7fe05cf85fd0
rbsejin <rbsejin@gmail.com>
2021-02-25 11:26:24 +0900

    잠금화면에서도 알림, 진동 구현

--- app/src/main/java/com/example/facemaker/AlarmReceiver.kt
+++ app/src/main/java/com/example/facemaker/AlarmReceiver.kt
@@ -1,14 +1,21 @@
 package com.example.facemaker
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Context.VIBRATOR_SERVICE
 import android.content.Intent
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
 import androidx.core.app.NotificationCompat
 
+
 class AlarmReceiver : BroadcastReceiver() {
+    @SuppressLint("WrongConstant")
     override fun onReceive(context: Context?, intent: Intent?) {
         val notificationManager: NotificationManager =
             context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
@@ -21,6 +28,7 @@ class AlarmReceiver : BroadcastReceiver() {
         val builder: NotificationCompat.Builder = NotificationCompat.Builder(context, "default")
 
         builder.setSmallIcon(R.drawable.ic_launcher_foreground)
+
         // OREO API 26 이상부터 채널 필요
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             val channel =
@@ -53,6 +61,15 @@ class AlarmReceiver : BroadcastReceiver() {
             .setContentText(task.content)
             .setContentInfo("INFO")
             .setContentIntent(pendingIntent)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+
+        val vibrator = context.getSystemService(VIBRATOR_SERVICE) as Vibrator
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            vibrator.vibrate(1000);
+        }
 
         notificationManager.notify(task.id, builder.build())
     }
