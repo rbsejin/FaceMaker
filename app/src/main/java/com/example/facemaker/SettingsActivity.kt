6950a5fb4377c735d619f68ed82c583ee53d13a3
rbsejin <rbsejin@gmail.com>
2021-03-10 22:35:51 +0900

    회원 탈퇴할 때 DB에서 project, task DB도 삭제

--- app/src/main/java/com/example/facemaker/SettingsActivity.kt
+++ app/src/main/java/com/example/facemaker/SettingsActivity.kt
@@ -7,12 +7,17 @@ import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import timber.log.Timber
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +29,9 @@ class SettingsActivity : AppCompatActivity() {
                 .commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        database = Firebase.database.reference
+        auth = Firebase.auth
 
         val prefs = PreferenceManager.getDefaultSharedPreferences(this)
 
@@ -46,8 +54,40 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     fun deleteAccount() {
+        val userId = auth.currentUser.uid
+
+        // 작업 삭제
+        database.child("tasks").get().addOnSuccessListener {
+            val ids = mutableListOf<String>()
+            for (snapshot in it.children) {
+                if (snapshot.child("userId").getValue<String>() == userId) {
+                    snapshot.key?.let { id ->
+                        ids.add(id)
+                    }
+                }
+            }
+
+            val childUpdates = ids.map { id -> "tasks/$id" to null }.toMap()
+            database.updateChildren(childUpdates)
+        }
+
+        // 프로젝트 삭제
+        database.child("projects").get().addOnSuccessListener {
+            val ids = mutableListOf<String>()
+            for (snapshot in it.children) {
+                if (snapshot.child("hostUserId").getValue<String>() == userId) {
+                    snapshot.key?.let { id ->
+                        ids.add(id)
+                    }
+                }
+            }
+
+            val childUpdates = ids.map { id -> "projects/$id" to null }.toMap()
+            database.updateChildren(childUpdates)
+        }
+
         // 계정 삭제
-        Firebase.database.reference.child("users").child(Firebase.auth.currentUser.uid)
+        database.child("users").child(userId)
             .removeValue()
 
         AuthUI.getInstance()
