f457c45f1f60fb5c97895cfdf1bc6971e3b50393
rbsejin <rbsejin@gmail.com>
2021-03-16 01:47:30 +0900

    다음단계 DB에 추가

--- app/src/main/java/com/example/facemaker/TaskStepAdapter.kt
+++ app/src/main/java/com/example/facemaker/TaskStepAdapter.kt
@@ -3,62 +3,59 @@ package com.example.facemaker
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.ImageView
+import android.view.inputmethod.EditorInfo
 import androidx.recyclerview.widget.RecyclerView
 import com.example.facemaker.data.Task
+import com.example.facemaker.databinding.StepItemBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class TaskStepAdapter(
     private var currentTask: Task,
 ) :
-    RecyclerView.Adapter<TaskStepAdapter.TaskStepViewHolder>() {
+    RecyclerView.Adapter<TaskStepAdapter.ViewHolder>() {
     private lateinit var parentContext: Context
     private val stepList: MutableList<String> = currentTask.stepList
 
-    class TaskStepViewHolder(
-        itemView: View
-    ) :
-        RecyclerView.ViewHolder(itemView) {
-        val iconImageView = itemView.findViewById<ImageView>(R.id.item_icon)
-        val nameTextView = itemView.findViewById<EditText>(R.id.item_name_text)
-        val deleteButton: ImageButton = itemView.findViewById(R.id.item_delete_button)
+    class ViewHolder(val binding: StepItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = StepItemBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskStepViewHolder {
-        parentContext = parent.context
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.step_item, parent, false)
-
-        return TaskStepViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
     }
 
-    override fun onBindViewHolder(holder: TaskStepViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         if (position == stepList.size) {
-            holder.deleteButton.visibility = RecyclerView.INVISIBLE
-            holder.iconImageView.setImageResource(R.drawable.baseline_add_24)
-            holder.nameTextView.hint = "다음단계"
-            holder.nameTextView.setText("")
+            holder.binding.itemDeleteButton.visibility = RecyclerView.INVISIBLE
+            holder.binding.itemIcon.setImageResource(R.drawable.baseline_add_24)
+            holder.binding.itemNameText.hint = "다음단계"
+            holder.binding.itemNameText.setText("")
         } else {
-            holder.deleteButton.visibility = RecyclerView.VISIBLE
-            holder.iconImageView.setImageResource(R.drawable.baseline_check_box_outline_blank_24)
-            holder.nameTextView.setText(stepList[position])
+            holder.binding.itemDeleteButton.visibility = RecyclerView.VISIBLE
+            holder.binding.itemIcon.setImageResource(R.drawable.baseline_check_box_outline_blank_24)
+            holder.binding.itemNameText.setText(stepList[position])
         }
 
-        holder.nameTextView.setOnFocusChangeListener { v, hasFocus ->
+        holder.binding.itemNameText.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {
                 Log.d("focus", "$position 포커스 잃음")
 
                 if (position == stepList.size) {
-                    val text: String = holder.nameTextView.text.toString()
+                    val text: String = holder.binding.itemNameText.text.toString()
                     if (text.isNotEmpty()) {
                         stepList.add(text)
-                        notifyDataSetChanged()
                     }
                 } else {
-                    val text: String = holder.nameTextView.text.toString()
+                    val text: String = holder.binding.itemNameText.text.toString()
                     if (text.isEmpty()) {
                         stepList.removeAt(position)
                         notifyDataSetChanged()
@@ -69,23 +66,33 @@ class TaskStepAdapter(
             } else {
                 Log.d("focus", "$position 포커스 얻음")
             }
+
+            Firebase.database.reference.child("tasks/${currentTask.id}/stepList").setValue(stepList)
         }
 
-        holder.nameTextView.setOnEditorActionListener { v, actionId, event ->
-            holder.nameTextView.clearFocus()
+        holder.binding.itemNameText.setOnEditorActionListener { v, actionId, event ->
+            when (actionId) {
+                EditorInfo.IME_ACTION_DONE -> {
+                    holder.binding.itemNameText.clearFocus()
+                    true
+                }
+                else -> false
+            }
+
+            Firebase.database.reference.child("tasks/${currentTask.id}/stepList").setValue(stepList)
             true
         }
 
         // 삭제 버튼 클릭했을 때
-        holder.deleteButton.setOnClickListener {
+        holder.binding.itemDeleteButton.setOnClickListener {
             // 파일 추가 아이템에서는 삭제버튼을 클릭 불가
             assert(position < stepList.size)
 
-            holder.nameTextView.clearFocus()
+            holder.binding.itemNameText.clearFocus()
             // 파일 아이템 삭제한다.
-            currentTask.stepList.removeAt(position)
+            stepList.removeAt(position)
 
-            notifyDataSetChanged()
+            Firebase.database.reference.child("tasks/${currentTask.id}/stepList").setValue(stepList)
         }
     }
 
